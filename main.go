82120b0650452d5fc196844017982b8849939628
bihellzin <oliva.gabriel@outlook.com>
2023-08-27 01:13:46 -0300

    add highlight to expression

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 	}
 
 	expression := args[1]
+	expressionLength := len(expression)
 	filePath := args[2]
 	file, err := os.Open(filePath)
 
@@ -43,22 +44,38 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	// red := "\033[31m"
-	// bold := "\033[1m"
-	// reset := "\033[0m"
-	// green := "\033[32m"
+	red := "\033[31m"
+	bold := "\033[1m"
+	reset := "\033[0m"
 
 	// fmt.Printf("%sTexto em vermelho e negrito%s\n", red+bold, reset)
 
 	// coloredText := fmt.Sprintf("%sTexto vermelho%s e %sTexto verde%s", red+bold, reset, green, reset)
 	// fmt.Println(coloredText)
-
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, expression) {
-			fmt.Println(line)
-			// positions := findAllOccurrences(line, expression)
-			// fmt.Println(positions)
+			concat := ""
+			n := 0
+			positions := findAllOccurrences(line, expression)
+
+			position := positions[n]
+
+			for index, value := range line {
+				if position == index {
+					concat += red + bold
+				}
+				concat += string(value)
+
+				if index >= position + expressionLength - 1 {
+					concat += reset
+					if n < len(positions) - 1 {
+						n++
+						position = positions[n]
+					}
+				}
+			}
+			fmt.Println(concat)
 		}
 	}
 
