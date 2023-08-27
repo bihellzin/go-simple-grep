c5031ba9266fc18b0f4957b07c3bd7a9b42be5de
bihellzin <oliva.gabriel@outlook.com>
2023-08-27 00:00:26 -0300

    using args to pass filepath

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
+
+func findAllOccurrences(mainString, substring string) []int {
+	positions := []int{}
+	start := 0
+
+	for {
+		index := strings.Index(mainString[start:], substring)
+		if index == -1 {
+			break
+		}
+		positions = append(positions, start+index)
+		start = start + index + len(substring)
+	}
+
+	return positions
+}
 
 func main() {
 	args := os.Args
@@ -14,19 +31,38 @@ func main() {
 		return
 	}
 
-	filePath := "sample.txt" // Change this to the path of your file
-	file, err := os.ReadFile(filePath)
+	expression := args[1]
+	filePath := args[2]
+	file, err := os.Open(filePath)
 
-	fileData := string(file)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
-	// fmt.Print(string(file))
+	defer file.Close()
 
-	if strings.Contains(fileData, args[1]) {
-		fmt.Printf("the term \"%s\" is present in file %s", args[1], filePath)
-	} else {
-		fmt.Printf("the term \"%s\" is not present in file %s", args[1], filePath)
+	scanner := bufio.NewScanner(file)
+
+	// red := "\033[31m"
+	// bold := "\033[1m"
+	// reset := "\033[0m"
+	// green := "\033[32m"
+
+	// fmt.Printf("%sTexto em vermelho e negrito%s\n", red+bold, reset)
+
+	// coloredText := fmt.Sprintf("%sTexto vermelho%s e %sTexto verde%s", red+bold, reset, green, reset)
+	// fmt.Println(coloredText)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, expression) {
+			fmt.Println(line)
+			// positions := findAllOccurrences(line, expression)
+			// fmt.Println(positions)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading the file:", err)
 	}
 }
